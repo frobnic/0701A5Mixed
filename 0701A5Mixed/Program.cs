07410c043c58f1304b28f27b8416d372296c80be
frank paulsen <frobnic+ita@gmail.com>
2019-08-28 14:04:56 +0200

    Projektdateien hinzufügen.

---
+++ 0701A5Mixed/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0701A5Mixed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
